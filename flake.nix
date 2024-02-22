952ae81b2212101ddfe6f8d730705080f52bfd51
Robert James Hernandez <rob@sarcasticadmin.com>
2024-02-22 06:08:47 +0000

    nix: include configuration.nix in image
    There is not normally a `/etc/nixos/configuration.nix`. This allows us
    to include the very same config.nix from this repo in the vm image that
    we are creating. It allows users to easily configure the vm and
    nixos-rebuild switch

--- flake.nix
+++ flake.nix
@@ -12,9 +12,28 @@
       };
 
       nixosConfigurations = {
-        cola = nixpkgs.lib.nixosSystem {
+        cola = let
+          pkgs = import nixpkgs {
+            system = "x86_64-linux";
+          };
+          # ignore the first three and last lines of config.nix to include in image
+          # TODO: Make this less of a hack
+          truncConfig = pkgs.runCommand "truncate"
+          {
+            buildInputs = [ pkgs.util-linux pkgs.coreutils-full ];
+          }
+          ''
+            mkdir $out
+            awk 'NR>3' ${./config.nix} | head -n -1 > $out/config
+          '';
+          eConfig = (builtins.readFile "${truncConfig}/config");
+        in
+          nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           modules = [
+            # always for us to include a /etc/nixos/configuration.nix
+            ({ config, pkgs, ... }: { installer.cloneConfigExtra = eConfig ; })
+            "${nixpkgs}/nixos/modules/profiles/clone-config.nix"
             "${nixpkgs}/nixos/modules/virtualisation/kubevirt.nix"
             ./config.nix
           ];
